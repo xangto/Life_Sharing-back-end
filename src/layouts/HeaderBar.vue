ccd8f52a5f61c785ee0dc728cbd3cb30b17650c3
wangtao <2281659624@qq.com>
2026-08-19 10:45:10 +0800

    feat: github跳转

--- src/layouts/HeaderBar.vue
+++ src/layouts/HeaderBar.vue
@@ -10,11 +10,17 @@
           {{ avatarText }}
         </el-avatar>
         <span class="text-sm text-gray-700">{{ auth.username || '管理员' }}</span>
-        <el-icon><ArrowDown /></el-icon>
+        <el-icon><ArrowDown/></el-icon>
       </span>
       <template #dropdown>
         <el-dropdown-menu>
-          <el-dropdown-item command="password" :icon="Lock">修改密码</el-dropdown-item>
+          <el-dropdown-item command="github">
+            <template #icon>
+              <Icon icon="mdi:github" size="18"/>
+            </template>
+            <el-link href="https://github.com/xangto" target="_blank">github</el-link>
+          </el-dropdown-item>
+          <el-dropdown-item command="password" divided :icon="Lock">修改密码</el-dropdown-item>
           <el-dropdown-item command="logout" divided :icon="SwitchButton">退出登录</el-dropdown-item>
         </el-dropdown-menu>
       </template>
@@ -23,11 +29,12 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import { ElMessageBox } from 'element-plus'
-import { ArrowDown, Lock, SwitchButton } from '@element-plus/icons-vue'
-import { useAuthStore } from '@/stores/auth'
+import {computed} from 'vue'
+import {useRoute, useRouter} from 'vue-router'
+import {ElMessageBox} from 'element-plus'
+import {ArrowDown, Lock, SwitchButton} from '@element-plus/icons-vue'
+import {useAuthStore} from '@/stores/auth'
+import {Icon} from '@iconify/vue'
 
 const route = useRoute()
 const router = useRouter()
@@ -40,7 +47,7 @@ async function handleCommand(command: string | number | object) {
     router.push('/password')
   } else if (command === 'logout') {
     try {
-      await ElMessageBox.confirm('确定退出登录吗？', '提示', { type: 'warning' })
+      await ElMessageBox.confirm('确定退出登录吗？', '提示', {type: 'warning'})
     } catch {
       return
     }
